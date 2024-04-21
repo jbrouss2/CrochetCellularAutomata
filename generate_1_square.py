f564e5f30241bbeebd3d87ef765f8cfc4729a895
Juliana Broussard <jbrouss2@vols.utk.edu>
2024-04-21 16:11:52 -0400

    Allow timesteps, k changed by user

--- generate_1_square.py
+++ generate_1_square.py
@@ -7,12 +7,14 @@ from PIL import Image
 import os
 
 class PatternGenerator():
-    def __init__(self, size_block, number_of_blocks, size_pattern, neighbourhood, rule_number):
+    def __init__(self, size_block, number_of_blocks, size_pattern, neighbourhood, rule_number, timesteps, k_value):
         self.size_block = size_block
         self.number_of_blocks = number_of_blocks
         self.size_pattern = size_pattern
         self.neighbourhood = neighbourhood
         self.rule_number = rule_number
+        self.timesteps = timesteps
+        self.k_value = k_value
 
     # Compile the info to generate one sqaure
     def generate_square(self):
@@ -31,8 +33,8 @@ class PatternGenerator():
 
         # print(neighborhood)
 
-        pattern_block = cpl.evolve2d(pattern_block_size, timesteps=30, neighbourhood=neighborhood,
-                                            apply_rule=lambda n, c, t: cpl.totalistic_rule(n, k=2, rule=self.rule_number))
+        pattern_block = cpl.evolve2d(pattern_block_size, timesteps=self.timesteps, neighbourhood=neighborhood,
+                                            apply_rule=lambda n, c, t: cpl.totalistic_rule(n, k=self.k_value, rule=self.rule_number))
  
         cpl.plot2d(pattern_block, show_grid=True)
         return pattern_block
@@ -48,9 +50,11 @@ if __name__ == '__main__':
     # Arguments for the Cellular Automata Formula
     parser.add_argument("--neighbourhood", "-n", required=False, type=int, help="0 for Moore, 1 for von Neumann", default=1)
     parser.add_argument("--rule_number", "-r", required=True, type=int, help= "Rule number")
+    parser.add_argument("--timesteps", "-t", required=False, type=int, help="Timesteps", default=30)
+    parser.add_argument("--k_value", "-k", required=False, type=int, help="K value", default=2)
 
     args = parser.parse_args()
     print("Arguments:", args)
 
-    pattern = PatternGenerator(args.size_block, args.number_of_blocks, args.size_pattern, args.neighbourhood, args.rule_number)
+    pattern = PatternGenerator(args.size_block, args.number_of_blocks, args.size_pattern, args.neighbourhood, args.rule_number, args.timesteps, args.k_value)
     pattern.generate_square()
